7834485101c6bbc6cbc98fae00f9b884bf4b49e0
mdiktushar <md.tushar1804@gmail.com>
2020-04-06 19:41:15 +0600

    com2 6/4/20

--- 158A/158A.cpp
+++ 158A/158A.cpp
@@ -3,17 +3,27 @@ using namespace std;
 
 int main()
 {
-    int n,k;cin>>n>>k;
-    int arr[n];
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+    int n; cin>>n;
+    vector<int>v;
+    int a=0,b=0,c=0,d=0;
+    while(n--){
+        int i;cin>>i;
+        if(i==1)a++;
+        else if(i==2)b++;
+        else if(i==3)c++;
+        else d++;
+        if(b==2){d++;b=0;}
     }
-    int f=0;
-    for(int i=0; i<n; i++){
-        if(arr[i]>=arr[k-1] && (arr[k-1] != 0 || arr[i] != 0))
-            f++;
+    if(a<c)a==0;
+    else a-=c;
+    if(b>0){
+        if(a>2)a-=2;
+        else a=0;
     }
-    cout<<f<<endl;
+    int temp = a%4;
+    a=a/4;
+    if(temp>0)a++;
+    cout<<a+b+c+d<<endl;
 
     return 0;
 }
