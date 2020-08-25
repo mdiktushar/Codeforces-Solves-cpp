a40e3552d9e9dab1b522f065f0db980b2863698d
Md. Imrul Kayes <md.tushar1804@gmail.com>
2020-08-25 21:16:00 +0600

    25/8/20 commit-1

---
+++ 1392A/1392A.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#include<cstdlib>
+#include <algorithm>
+#include <functional>
+#include <vector>
+
+#define lli long long int
+
+using namespace std;
+
+int Sum(int n){
+    if(n == 1)
+        return 1;
+    else return n+Sum(n-1);
+}
+
+void zeroarr(int arr[], int n){
+    for(int i=0; i<n; i++){
+        arr[i] = 0;
+    }
+}
+
+lli findSum(int A[], int N) {
+    if (N <= 0)
+        return 0;
+    return (findSum(A, N - 1) + A[N - 1]);
+}
+
+void viewarr(lli arr[], lli n){
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<' ';
+    cout<<endl;
+}
+
+void viewarr(int arr[], int n){
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<' ';
+    cout<<endl;
+}
+
+
+//md.imrul kayes
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        lli n;cin>>n;
+        set<lli>st;
+        for(lli j=0; j<n; j++)
+        {
+            lli i;cin>>i;
+            st.insert(i);
+
+        }
+        if(st.size()==1)
+            cout<<n<<endl;
+        else
+            cout<<1<<endl;
+    }
+    return 0;
+}
+
