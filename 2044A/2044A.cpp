e5666856ade3327098317f273a0e9514c3db07d8
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2024-12-17 13:48:22 +0600

    commit -1 17-12-24

---
+++ 2044A/2044A.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#include<cstdlib>
+#include <algorithm>
+#include <functional>
+#include <vector>
+
+#define lli long long int
+#define ulli unsigned long long int
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+
+using namespace std;
+
+int Sum(int n){
+    if(n == 1)
+        return 1;
+    else return n+Sum(n-1);
+}
+
+void zeroarr(int arr[], int n){
+    for(int i=0; i<n; i++){
+        arr[i] = 0;
+    }
+}
+
+lli findSum(int A[], int N) {
+    if (N <= 0)
+        return 0;
+    return (findSum(A, N - 1) + A[N - 1]);
+}
+
+void viewarr(lli arr[], lli n){
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<' ';
+    cout<<endl;
+}
+
+void viewarr(int arr[], int n){
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<' ';
+    cout<<endl;
+}
+
+//cin.ignore();
+//getline(cin,s);
+
+//md.imrul kayes
+
+int main()
+{
+    lli _;
+    cin>>_;
+    while(_--)
+    {
+        int n;cin>>n;
+        cout<<--n<<endl;
+    }
+
+    return 0;
+}
