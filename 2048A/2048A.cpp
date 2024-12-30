ef959792054417bc2d9f89ebe717d7e288372216
Md. Imrul Kayes <48817876+mdiktushar@users.noreply.github.com>
2024-12-30 13:59:54 +0600

    commit-1 30-12-24

---
+++ 2048A/2048A.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+#include<cstdlib>
+#include <algorithm>
+#include <functional>
+#include <vector>
+
+#define lli long long int
+#define ulli unsigned long long int
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+
+using namespace std;
+
+int Sum(int n){
+    if(n == 1)
+        return 1;
+    else return n+Sum(n-1);
+}
+
+void zeroarr(int arr[], int n){
+    for(int i=0; i<n; i++){
+        arr[i] = 0;
+    }
+}
+
+lli findSum(int A[], int N) {
+    if (N <= 0)
+        return 0;
+    return (findSum(A, N - 1) + A[N - 1]);
+}
+
+void viewarr(lli arr[], lli n){
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<' ';
+    cout<<endl;
+}
+
+void viewarr(int arr[], int n){
+    for(int i=0; i<n; i++)
+        cout<<arr[i]<<' ';
+    cout<<endl;
+}
+
+//cin.ignore();
+//getline(cin,s);
+
+//md.imrul kayes
+
+int main() {
+    lli _;
+    cin >> _;
+    while (_--) {
+        lli n;
+        cin >> n;
+        if (n < 33) {
+            NO;
+            continue;
+        }
+        while (n >= 33) {
+            if (n % 33 == 0) {
+                YES;
+                break;
+            }
+            string s = to_string(n);
+            int f = s.find("33");
+            if (f == string::npos) {
+                NO;
+                break;
+            }
+            s.erase(f, 2);
+            n = stoi(s);
+            if (n < 33) {
+                NO;
+                break;
+            }
+        }
+    }
+    return 0;
+}
